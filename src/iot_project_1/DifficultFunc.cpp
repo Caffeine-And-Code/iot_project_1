a468c880b98b2a9b33ca5a2bd4f9dac608cce033
Fabio Fattori <fabio.fattori3@studio.unibo.it>
2024-10-09 13:31:10 +0200

    Added Testing Function

--- src/iot_project_1/DifficultFunc.cpp
+++ src/iot_project_1/DifficultFunc.cpp
@@ -1,6 +1,12 @@
 #include "DifficultFunc.hpp"
 
+using namespace std;
+
 #define NUMBERS_OF_DIFFICULTIES 4
+// 10 secondi di dafault
+#define MILLISECONDS_STARTING_VALUE 10000
+
+#define ANALOG_MAX_VALUE 1023
 
 int difficulty = 0;
 
@@ -9,7 +15,7 @@ int getDifficult() {
 }
 
 void setDifficult(int value) {
-    if (value >= 0){
+    if (value >= 0 && value <= ANALOG_MAX_VALUE) {
         difficulty = value % NUMBERS_OF_DIFFICULTIES;
     }
 }
